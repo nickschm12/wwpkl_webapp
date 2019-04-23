c1e4f27f6759336303687b7ff2383cf928fb33db
Nicholas Schmidt <nschmidt@Icculus.local>
2019-04-23 10:13:26 -0700

    Forgot to move back to the AWS DB

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-#SQLALCHEMY_DATABASE_URI = 'postgresql://nickschm12:Harper_3@wwpkldb.cogkhvjditiu.us-west-1.rds.amazonaws.com/wwpkl_db'
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/wwpkl'
+SQLALCHEMY_DATABASE_URI = 'postgresql://nickschm12:Harper_3@wwpkldb.cogkhvjditiu.us-west-1.rds.amazonaws.com/wwpkl_db'
+#SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/wwpkl'
 SQLALCHEMY_POOL_RECYCLE = 3600
